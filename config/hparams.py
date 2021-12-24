46b11aaf68a1a26a11f60e1127b8a1b865cbf12c
Arthur Zucker <arthur.zucker@gmail.com>
2021-12-24 17:31:25 +0100

    PEP foramt base.py

--- config/hparams.py
+++ config/hparams.py
@@ -5,6 +5,7 @@ from posixpath import split
 from typing import List, Dict
 import numpy as np
 from simple_parsing.helpers import dict_field, list_field
+import torch
 
 """Dataclass allows to have arguments and easily use wandb's weep module.
 
@@ -23,16 +24,28 @@ class hparams:
     # projectname
     wandb_project: str = "test-sem-seg"
     # seed
-    seed_everything: float = np.random.randint(10000)
+    seed_everything: float = 40 #np.random.randint(10000)
     # maximum number of epochs
     max_epochs: int = 40
     # path to download pascal voc
     asset_path: str = osp.join(os.getcwd(), "assets")
-    # ignore class 
+    # ignore class
     ignore_index: int = 21
     # loss to train the model
-    loss: Dict[str, Dict[str, str]] = dict_field(dict(torch_nn_CrossEntropyLoss=
-                                                        dict(ignore_index=ignore_index)))
+    loss: Dict[str, Dict[str, str]] = dict_field(
+        dict(
+            torch_nn_CrossEntropyLoss=dict(
+                ignore_index=ignore_index,
+                weight=torch.cat((torch.tensor([0.5]), torch.ones(21))),
+            )
+        )
+    )
+    # loss: Dict[str, Dict[str, str]] = dict_field(
+    #     dict(
+    #         models_losses_diceloss_DiceLoss=dict(
+    #         )
+    #     )
+    # )
     # learning rate
     lr: float = 0.02089296130854041
     # agent to use for training
@@ -48,7 +61,7 @@ class hparams:
     # use bilinear interpolation
     bilinear: bool = True
     # batch size for training
-    batch_size: int = 2
+    batch_size: int = 16
     # split value
     split_val: float = 0.2
     # validation frequency
@@ -60,35 +73,51 @@ class hparams:
     # save directory
     save_dir: str = osp.join(os.getcwd(), "wandb")
     # number of workers for dataloaders
-    num_workers: int = 1
+    num_workers: int = 16
     # tune the model on first run
     tune: bool = False
     # number or gpu
-    gpu: int = 0
+    gpu: int = 1
     # precision
-    precision: int = 16
+    precision: int = 32
     # effective receptive fields log frequency
     erf_freq: int = 20
     # index of the layers to use for the receptive field visualization
-    layers: List[int] = list_field(64,80,95)
-    # metrics
-    metrics: Dict[str, Dict[str, str]] = dict_field(dict(Accuracy=dict(
-        num_classes=n_classes, average="weighted", mdmc_average='global', ignore_index=ignore_index
-    ),
-        Recall=dict(
-            num_classes=n_classes, average="weighted", mdmc_average='global', ignore_index=ignore_index
-    ),
-        Precision=dict(
-            num_classes=n_classes, average="weighted", mdmc_average='global', ignore_index=ignore_index
-    ),
-    #     AveragePrecision=dict(
-    #         num_classes=n_classes, average="weighted", ignore_index=ignore_index
-    # ),
-        IoU=dict(
-            num_classes=n_classes, ignore_index=ignore_index
-    )))
-        # ConfusionMatrix=dict()))
+    layers: List[int] = list_field(64, 80, 95)
+    # metrics
+    metrics: Dict[str, Dict[str, str]] = dict_field(
+        dict(
+            Accuracy=dict(
+                num_classes=n_classes,
+                average="weighted",
+                mdmc_average="global",
+                ignore_index=ignore_index,
+            ),
+            Recall=dict(
+                num_classes=n_classes,
+                average="weighted",
+                mdmc_average="global",
+                ignore_index=ignore_index,
+            ),
+            Precision=dict(
+                num_classes=n_classes,
+                average="weighted",
+                mdmc_average="global",
+                ignore_index=ignore_index,
+            ),
+            F1=dict(
+                num_classes=n_classes,
+                average="weighted",
+                mdmc_average="global",
+                ignore_index=ignore_index,
+            ),
+            #     AveragePrecision=dict(
+            #         num_classes=n_classes, average="weighted", ignore_index=ignore_index
+            # ),
+            IoU=dict(num_classes=n_classes, ignore_index=ignore_index),
+        )
+    )
+    # ConfusionMatrix=dict()))
 
     # optimizer
-    optimizer: Dict[str, Dict[str, str]] = dict_field(dict(torch_optim_SGD=
-                                                        dict()))
+    optimizer: Dict[str, Dict[str, str]] = dict_field(dict(torch_optim_AdamW=dict()))
