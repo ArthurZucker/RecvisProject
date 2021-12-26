4d3536befdc78df6e760c5c5248f1901ec801981
APAVOU Clément <45719060+clementapa@users.noreply.github.com>
2021-12-26 14:41:39 +0100

    remove mean and std and .detach on image

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -54,9 +54,7 @@ class LogPredictionsCallback(Callback):
 
         for i in range(n):
 
-            bg_image = images[i].detach().numpy().transpose((1, 2, 0))
-            mean = np.array([0.485, 0.456, 0.406])
-            std = np.array([0.229, 0.224, 0.225])
+            bg_image = images[i].numpy().transpose((1, 2, 0))
             bg_image = std * bg_image + mean
             bg_image = np.clip(bg_image, 0, 1)
 
