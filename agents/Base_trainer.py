3d899f4326bfce676969bc3482d8a82c8655b481
Arthur Zucker <arthur.zucker@gmail.com>
2022-01-09 06:58:24 +0100

    Updated base trainer with new hparam structure
    Co-authored-by: APAVOU Clément <clementapa@users.noreply.github.com>

--- agents/Base_trainer.py
+++ agents/Base_trainer.py
@@ -5,12 +5,16 @@ from utils.logger import init_logger
 
 class BaseTrainer:
     def __init__(self, config, run) -> None:
-        self.config = config
+        self.config = config.hparams
         self.wb_run = run
-        self.model = get_net(config)
+        self.encoder = config.network_param.backbone
+        self.model = get_net(
+            config.hparams.arch, config.network_param, config.optim_param
+        )
         self.wb_run.watch(self.model)
-        self.datamodule = get_datamodule(config)
-        self.logger = init_logger("Trainer", "DEBUG")
+        self.datamodule = get_datamodule(
+            config.hparams.datamodule, config.data_param,config.hparams.dataset
+        )
 
     def run(self):
         
@@ -20,16 +24,18 @@ class BaseTrainer:
                 gpus=self.config.gpu,
                 auto_lr_find=True,
                 accelerator="auto",
+                default_root_dir = self.wb_run.save_dir
             )
             trainer.logger = self.wb_run
             trainer.tune(self.model, datamodule=self.datamodule)
-        
+
         if self.config.tune_batch_size:
             trainer = pl.Trainer(
                 logger=self.wb_run,
                 gpus=self.config.gpu,
                 auto_scale_batch_size="power",
                 accelerator="auto",
+                default_root_dir = self.wb_run.save_dir
             )
             trainer.logger = self.wb_run
             trainer.tune(self.model, datamodule=self.datamodule)
