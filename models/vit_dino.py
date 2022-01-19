312acf85f91abe4ca814f31b5fc33a54c7ff3550
Arthur Zucker <arthur.zucker@gmail.com>
2022-01-20 00:13:44 +0100

    no pretrain

--- models/vit_dino.py
+++ models/vit_dino.py
@@ -2,6 +2,6 @@ import timm
 from timm.models.vision_transformer import default_cfgs
 def vit_dino(vit_parameters):
     
-    model =  timm.create_model('vit_small_patch8_224_dino', pretrained=True)
+    model =  timm.create_model('vit_small_patch8_224_dino', pretrained=False)
     model.backbone_parameters = default_cfgs['vit_small_patch8_224_dino']
     return model
